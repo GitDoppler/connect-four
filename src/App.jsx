713eae891aa03705b253689937aef0611517a679
GitDoppler <carabatraducu@gmail.com>
2023-08-02 22:00:34 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
     return (
         <div className=" text-center">
             <h1 className="text-2xl text-black">Header 1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis laudantium, facere unde saepe suscipit vel laboriosam odio, dolor adipisci placeat earum autem itaque dicta maiores dolorem omnis maxime quod vero.</p>
         </div>
     )
 }
