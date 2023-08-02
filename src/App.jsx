f45d8a4c64b98c0e05ff4841d6e97732cbfa1b3d
GitDoppler <carabatraducu@gmail.com>
2023-08-02 21:35:26 +0300

    Initializing the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className=" text-center">
+            <h1 className="text-2xl text-black">Header 1</h1>
+        </div>
+    )
+}
+
+export default App
