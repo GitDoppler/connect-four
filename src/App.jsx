bd40e00bff0375b25381479eb9f7de2ac77466fa
GitDoppler <carabatraducu@gmail.com>
2023-08-03 17:52:44 +0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import './App.css'
-
 function App() {
-    return (
-        <div className=" text-center">
-            <h1 className="text-2xl text-black">Header 1</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis laudantium, facere unde saepe suscipit vel laboriosam odio, dolor adipisci placeat earum autem itaque dicta maiores dolorem omnis maxime quod vero.</p>
-        </div>
-    )
+    return <div className=" min-h-screen bg-purple-600">adwa</div>
 }
 
 export default App
