14e88a6a7354f6a7577fb6c5b66f4a5d74efc03f
GitDoppler <carabatraducu@gmail.com>
2023-08-30 16:17:52 +0300

    Cleaned the code for board.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,7 +6,7 @@ import WhiteBoardSmall from '../assets/images/board-layer-white-small.svg'
 import BlackBoardSmall from '../assets/images/board-layer-black-small.svg'
 import WhiteBoardLarge from '../assets/images/board-layer-white-large.svg'
 import BlackBoardLarge from '../assets/images/board-layer-black-large.svg'
-import RedCircle from '../assets/images/counter-red-large.svg'
+import RedPuck from '../assets/images/counter-red-large.svg'
 import { motion } from 'framer-motion'
 
 export default function Board() {
@@ -40,15 +40,15 @@ export default function Board() {
             </div>
 
             <div className="flex justify-center gap-5 pt-12 font-bold">
-                <div className="relative rounded-2xl border-4 border-black bg-white shadow-[0_10px_0_0_#000]">
-                    <div className="flex w-[clamp(8.88rem,calc(1.12rem+33.08vw),17.00rem)] flex-col items-center justify-center px-9 py-2 uppercase">
+                <div className="relative w-[42.388%] rounded-2xl border-4 border-black bg-white shadow-[0_10px_0_0_#000]">
+                    <div className="flex  flex-col items-center justify-center px-9 py-2 uppercase">
                         <div>Player 1</div>
                         <div className="text-3xl">{scoreP1}</div>
                     </div>
                     <img src={Player1} alt="Player 1" className="absolute left-[-1.6875rem] top-[10%] h-[3.6875rem] w-[3.375rem]" />
                 </div>
-                <div className="relative rounded-2xl border-4 border-black bg-white shadow-[0_10px_0_0_#000]">
-                    <div className="flex w-[clamp(8.88rem,calc(1.12rem+33.08vw),17.00rem)] flex-col items-center justify-center px-9 py-2 uppercase">
+                <div className="relative w-[42.388%] rounded-2xl border-4 border-black bg-white shadow-[0_10px_0_0_#000]">
+                    <div className="flex flex-col items-center justify-center px-9 py-2 uppercase">
                         <div>Player 2</div>
                         <div className="text-3xl">{scoreP1}</div>
                     </div>
@@ -56,15 +56,12 @@ export default function Board() {
                 </div>
             </div>
 
-            <div className="relative mx-auto mt-16 flex w-[clamp(20.94rem,calc(3.23rem+75.57vw),39.50rem)] flex-wrap justify-center gap-[clamp(0.81rem,calc(0.16rem+2.80vw),1.50rem)]">
+            <div className=" relative mx-auto mt-16 flex aspect-[1.25219/1] w-full flex-wrap items-center justify-center gap-[3.797%] md:gap-[24px]">
                 {matrix.map((row, indexRow) =>
                     row.map((e, indexCol) => (
-                        <div
-                            className="relative h-[clamp(2.13rem,calc(0.34rem+7.63vw),4.00rem)] w-[clamp(2.13rem,calc(0.34rem+7.63vw),4.00rem)] md:h-[64px] md:w-[64px]"
-                            key={indexRow + '-' + indexCol}
-                        >
+                        <div className="relative aspect-square w-[10.1492%] md:w-[64px]" key={indexRow + '-' + indexCol}>
                             <button
-                                className=" relative z-30 h-full w-full rounded-full md:h-[64px] md:w-[64px]"
+                                className="relative z-30 block h-full w-full rounded-full"
                                 onClick={() => {
                                     console.log(indexRow + '-' + indexCol)
                                     updateMatrix(indexRow, indexCol, 1)
@@ -72,19 +69,12 @@ export default function Board() {
                                 }}
                             ></button>
 
-                            {matrix[indexRow][indexCol] === 1 ? (
-                                <motion.img
-                                    initial={{ top: '-3.125rem' }}
-                                    animate={{ top: 'clamp(0.00rem, calc(-0.36rem + 1.53vw), 0.38rem)' }}
-                                    src={RedCircle}
-                                    className={'absolute z-10 aspect-square w-[calc(100%+8px)] max-w-none ' + `left-[${indexCol === 6 || indexCol === 5 ? '-4.5px' : '-3px'}]`}
-                                />
-                            ) : null}
+                            {matrix[indexRow][indexCol] === 1 ? <motion.img initial={{ top: '-120%' }} animate={{ top: '0px' }} src={RedPuck} className={`absolute left-[calc(50%-calc((100%+0.5rem)/2))] z-10 aspect-square w-[calc(100%+0.5rem)] max-w-none md:left-[-0.25rem]`} /> : null}
                         </div>
                     ))
                 )}
-                <img src={WhiteBoardSmall} alt="" className="absolute left-[0] top-[-0.625rem] z-20 w-[clamp(20.94rem,calc(3.23rem+75.57vw),39.50rem)] md:hidden"></img>
-                <img src={BlackBoardSmall} alt="" className="absolute left-0 top-[-0.625rem] z-0  w-[clamp(20.94rem,calc(3.23rem+75.57vw),39.50rem)] md:hidden"></img>
+                <img src={WhiteBoardSmall} alt="" className="absolute top-[-3.7%] z-20 w-full md:hidden"></img>
+                <img src={BlackBoardSmall} alt="" className="absolute top-[-3.7%] z-0 w-full md:hidden"></img>
                 <img src={WhiteBoardLarge} alt="" className="absolute left-0 top-[-20px] z-20 hidden md:block"></img>
                 <img src={BlackBoardLarge} alt="" className="absolute left-0 top-[-20px] z-0 hidden md:block"></img>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ export default {
                 'custom-dark-purple':'hsl(257,67%,51%)',
                 'custom-pink':'hsl(347,97%,70%)'
             },
+            spacing:{
+                'board' : 'clamp(20.94rem,calc(3.23rem + 75.57vw), 39.50rem)',
+                'puck' : 'calc(100% + 0.5rem)',
+                'button' : 'clamp(2.13rem,calc(0.34rem + 7.63vw), 4.00rem)',
+                'container-button' :'clamp(20.94rem,calc(3.23rem + 75.57vw), 39.50rem)',
+                'container-gap' : 'clamp(0.81rem,calc(0.16rem + 2.80vw), 1.50rem)',
+                'scoreboard' : 'clamp(8.88rem,calc(1.12rem + 33.08vw), 17.00rem)' 
+            }
         },
     },
     plugins: [],
